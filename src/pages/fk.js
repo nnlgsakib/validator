30b67c6a0969c6dc364bac879d1d7a0bed480f45
Your Name <you@example.com>
2023-06-15 21:01:57 +0600

    aa i  addr

---
+++ src/pages/fk.js
@@ -0,0 +1,31 @@
+export let  fakeApiData = [
+   
+      // Add more objects here with fake data
+      {
+        validator: "David Brown",
+        status: "Inactive",
+        validatorHash: "0xvwx234",
+        stakedMind: 1300,
+        earnedReward: 600,
+        totalValidatedBlock: 10000,
+      },
+      {
+        validator: "Olivia Green",
+        status: "Active",
+        validatorHash: "0xyza567",
+        stakedMind: 2800,
+        earnedReward: 1400,
+        totalValidatedBlock: 22000,
+      },
+     
+                  {
+                    validator: "Olivia Green",
+                    status: "Active",
+                    validatorHash: "0xyza567",
+                    stakedMind: 2800,
+                    earnedReward: 1400,
+                    totalValidatedBlock: 22000,
+                  },
+                  
+    // Add more objects here with fake data
+  ];
